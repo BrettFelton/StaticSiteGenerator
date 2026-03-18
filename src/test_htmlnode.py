747280880f4e8c4efd99df68aef7d30366f8b68d
Brett Felton <bmfelton@live.com>
2026-03-18 18:51:01 +0200

    Added leaf node

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -57,15 +57,29 @@ class TestHTMLNode(unittest.TestCase):
         }
         htmlnode = HTMLNode(props=test_props)
 
-        expected_result =  "href=\"https://www.google.com\" target=\"_blank\""
+        expected_result =  " href=\"https://www.google.com\" target=\"_blank\""
         self.assertEqual(expected_result, htmlnode.props_to_html())
 
     def test_not_eq_props_to_html(self):
         htmlnode = HTMLNode()
 
-        expected_result =  "href=\"https://www.google.com\" target=\"_blank\""
+        expected_result =  " href=\"https://www.google.com\" target=\"_blank\""
         self.assertNotEqual(expected_result, htmlnode.props_to_html())
 
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>',
+        )
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
 
 if __name__ == "__main__":
     unittest.main()
